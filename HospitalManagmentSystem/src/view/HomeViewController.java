244ba0ce2d28b6c55f59f41a8298e019c3917d02
Matt Padgett <mattpadgett@outlook.com>
2020-11-05 20:42:16 -0600

    Finished authentication and basic home page

---
+++ HospitalManagmentSystem/src/view/HomeViewController.java
@@ -0,0 +1,22 @@
+package view;
+
+import application.Main;
+import javafx.fxml.FXML;
+
+public class HomeViewController {
+	private Main main;
+	
+	@FXML
+	private void handleViewProfile() {
+		
+	}
+	
+	@FXML
+	private void handleLogout() {
+		
+	}
+	
+	public void setMain(Main main) {
+		this.main = main;
+	}
+}
