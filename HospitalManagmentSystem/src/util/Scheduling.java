5af57bbabf8b86e86c726788b36e6cc46c3af479
navaldes <Nicholas.Valdes2018@gmail.com>
2020-11-23 17:49:47 -0600

    temp

---
+++ HospitalManagmentSystem/src/util/Scheduling.java
@@ -0,0 +1,16 @@
+package util;
+
+import java.util.ArrayList;
+import java.sql.Date;
+
+public class Scheduling {
+  public static ArrayList<Date> getAvailability(int id) {
+    ArrayList<Date> times = new ArrayList<Date>(6);
+    
+    
+    
+    
+    return null;
+    
+  }
+}
